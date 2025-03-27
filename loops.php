2286fc92a2a1bf03948e761ce606294d9849e06b
Kim Raya <kimraya1017@gmail.com>
2025-03-27 14:20:41 +0800

    Added loops.php

---
+++ loops.php
@@ -0,0 +1,23 @@
+<?php
+
+// For loop
+$n = 25;
+
+// for ($i=1; $i <= $n; $i++){
+//     echo $i . '<br>';
+// }
+
+// While loop
+// $i = 1;
+// while ($i <= $n) {
+//     echo ("Loading..."). '<br>';
+//     $i++;
+// }
+// foreach loop
+$fruits = ["Apple", "Banana", "Cherry"];
+
+foreach ($fruits as $fruit) {
+    echo $fruit . " ";
+}
+
+?>
